a00547839e8db9101dd014b54bc4caff13989d7b
Barkley Santo <barkley.santo@gmail.com>
2021-04-27 14:47:04 -0500

    toEqual for object types

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
+// These are our tests - add, isNull...
 const functions = {
-  add: (num1, num2) => num1 + num2
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: "Barkley" }
+    user["lastName"] = "Santo";
+
+    return user
+  }
   // add: function (num1, num2) {
   //   return num1 + num2
   // }
 }
+
+// export the object
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,30 @@
 const functions = require('./functions');
-
+// toBe()
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+
+// .not() - to be falsy
 test('Adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+
+// isNull()
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// Should be falsy
+// feed null into check value method, expect it to be falsy- 
+// null is a false value.. so this passes
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// to be is for primitave types like numbers, an object is a reference type, so even though the objects look the same, they're actually different memory
+test('User should be Barkley Santo object', () => {
+  expect(functions.createUser()).toEqual({ firstName: "Barkley", lastName: "Santo" });
+});
+
